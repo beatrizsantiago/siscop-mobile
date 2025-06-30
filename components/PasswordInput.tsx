5bbed02c105af7e837224987d722f729b723a8d7
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2025-06-30 19:00:55 -0300

    ✨ feat: add PasswordInput component for secure password entry

---
+++ components/PasswordInput.tsx
@@ -0,0 +1,40 @@
+import Input from '@/components/Input';
+import theme from '@/theme';
+import Entypo from '@expo/vector-icons/Entypo';
+import { useState } from 'react';
+import { TextInputProps, View } from 'react-native';
+import { styled } from 'styled-components/native';
+
+const PasswordInput = ({ ...rest }:TextInputProps) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <View>
+      <Field
+        {...rest}
+        iconName="password"
+        placeholder="Senha"
+        secureTextEntry={!showPassword}
+      />
+      <IconButton onPress={() => setShowPassword((current) => !current)}>
+        {showPassword ? (
+          <Entypo name="eye-with-line" size={24} color={theme.gray.dark} />
+        ) : (
+          <Entypo name="eye" size={24} color={theme.gray.dark} />
+        )}
+      </IconButton>
+    </View>
+  );
+};
+
+const Field = styled(Input)`
+  padding-right: 48px;
+`;
+
+const IconButton = styled.TouchableOpacity`
+  position: absolute;
+  right: 0;
+  padding: 12px;
+`;
+
+export default PasswordInput;
