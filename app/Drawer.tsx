65752b758c04a6f4f7e2994a031e439056a2b266
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2025-06-30 21:12:40 -0300

    ✨ feat: enhance Drawer navigation with custom styles and dashboard icon

--- app/Drawer.tsx
+++ app/Drawer.tsx
@@ -2,6 +2,8 @@ import Header from '@/components/Header';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
+import theme from '@/theme';
+import { MaterialIcons } from '@expo/vector-icons';
 import DashboardScreen from './Dashboard';
 
 const Drawer = createDrawerNavigator();
@@ -12,11 +14,19 @@ const DrawerRoutes = () => (
       initialRouteName="Dashboard"
       screenOptions={{
         header: (props) => <Header {...props} />,
+        drawerActiveTintColor: theme.primary.main,
+        drawerItemStyle: {
+          borderRadius: 8,
+          borderWidth: 1,
+          borderStyle: 'solid',
+          borderColor: theme.primary.main,
+        },
       }}
     >
       <Drawer.Screen
         name="Dashboard"
         component={DashboardScreen}
+        options={{ drawerIcon: ({ color }) => <MaterialIcons name="dashboard" size={24} color={color} /> }}
       />
     </Drawer.Navigator>
   </GestureHandlerRootView>
