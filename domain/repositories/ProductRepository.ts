c6299fb7d41e0d1df8928659243b901757d52a29
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2025-07-03 21:43:55 -0300

    ✨ feat: create ProductRepository interface with CRUD operations

---
+++ domain/repositories/ProductRepository.ts
@@ -0,0 +1,10 @@
+import Product from '@/domain/entities/Product';
+
+export interface ProductRepository {
+  add(product: Product): Promise<Product>;
+  getAll(): Promise<Product[]>;
+  update(product: Product): Promise<Product>;
+  isProductReferenced(collectionName: string, productId: string): Promise<boolean>;
+  delete(id: string): Promise<void>;
+  searchByName(searchText: string): Promise<Product[]>;
+};
