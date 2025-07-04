3682dca11ef7754f172a9f8b733a44abe043ae0b
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2025-07-03 21:44:52 -0300

    ✨ feat: enhance Input component with customizable label, width, and margin options

--- components/Input.tsx
+++ components/Input.tsx
@@ -4,27 +4,43 @@ import { styled } from 'styled-components/native';
 
 type Props = TextInputProps & {
   iconName?: keyof typeof MaterialIcons.glyphMap;
+  withoutMargin?: boolean;
+  width?: string;
+  label?: string;
 };
 
-const Input = ({ iconName, ...rest }:Props) => (
-  <Container disabled={rest.editable === false}>
-    {iconName && (
-      <Icon name={iconName} size={24} />
+const Input = ({ iconName, withoutMargin = false, width = '100%', label, ...rest }:Props) => (
+  <>
+    {label && (
+      <Label>{label}</Label>
     )}
-    <Field {...rest} />
-  </Container>
+    <Container
+      disabled={rest.editable === false}
+      withoutMargin={withoutMargin}
+      width={width}
+      label={label}
+    >
+      {iconName && (
+        <Icon name={iconName} size={24} />
+      )}
+      <Field {...rest} />
+    </Container>
+  </>
 );
 
-const Container = styled.View<{ disabled: boolean }>`
-  width: 100%;
-  margin-bottom: 16px;
+const Container = styled.View<{
+  disabled: boolean, withoutMargin: boolean, width: string, label?: string
+  }>`
+  width: ${({ width }) => width};
+  height: ${({ label }) => label ? '40px' : 'auto'};
+  margin-bottom: ${({ withoutMargin }) => withoutMargin ? '0px' : '16px'};
   display: flex;
   flex-direction: row;
   align-items: center;
   border-bottom-width: 1.5px;
   border-bottom-color: ${({ theme }) => theme.primary.main};
   border-radius: 2px;
-  background-color: ${({ theme, disabled }) => disabled ? theme.gray.light : theme.high.main};
+  background-color: ${({ theme, disabled }) => disabled ? theme.gray.light : 'transparent'};
 `;
 
 const Field = styled(TextInput)`
@@ -36,6 +52,13 @@ const Icon = styled(MaterialIcons)`
   margin-right: 6px;
   margin-left: 6px;
   color: ${({ theme }) => theme.primary.main};
+`;
+
+const Label = styled.Text`
+  font-size: 14px;
+  color: ${({ theme }) => theme.primary.main};
+  font-weight: 500;
+  margin-top: 4px;
 `;
 
 export default Input;
