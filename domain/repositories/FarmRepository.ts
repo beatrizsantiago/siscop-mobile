30ba9cb8f89428f8386d8e6b4be7d668c9f129f7
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2025-07-05 11:46:10 -0300

    ✨ feat: create FarmRepository interface for farm management operations

---
+++ domain/repositories/FarmRepository.ts
@@ -0,0 +1,10 @@
+import Farm from '@/domain/entities/Farm';
+
+export interface FarmRepository {
+  add(farm: Farm): Promise<Farm>;
+  getAll(): Promise<Farm[]>;
+  update(farm: Farm): Promise<Farm>;
+  isFarmReferenced(collectionName: string, farmId: string): Promise<boolean>;
+  delete(id: string): Promise<void>;
+  searchByName(searchText: string): Promise<Farm[]>;
+};
