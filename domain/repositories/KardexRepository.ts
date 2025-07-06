2c9247959ee9d344e53f981e57d662a65476ac28
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2025-07-06 16:34:42 -0300

    ✨ feat: add methods for stock management in KardexRepository interface

--- domain/repositories/KardexRepository.ts
+++ domain/repositories/KardexRepository.ts
@@ -6,4 +6,7 @@ export interface KardexRepository {
   updateAmount(id: string, amount: number): Promise<void>;
   create(kardex: Kardex): Promise<void>;
   getProductsByState(state: string): Promise<ProducsByStateDataType>;
+  getAvailableAmount(farmId: string, productId: string): Promise<number>;
+  consumeStock(farmId: string, productId: string, amount: number): Promise<void>;
+  restoreStock(farmId: string, productId: string, amount: number): Promise<void>;
 };
