e4beef60d74368837932d934da0d2158e7ba01a8
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2025-07-05 22:01:54 -0300

    ✨ feat: add getProductsByState method to KardexRepository for product state retrieval

--- domain/repositories/KardexRepository.ts
+++ domain/repositories/KardexRepository.ts
@@ -1,7 +1,9 @@
 import Kardex from '@/domain/entities/Kardex';
+import { ProducsByStateDataType } from '@/types/chart';
 
 export interface KardexRepository {
   getByFarmProductState(farmId: string, productId: string, state: string): Promise<Kardex | null>;
   updateAmount(id: string, amount: number): Promise<void>;
   create(kardex: Kardex): Promise<void>;
+  getProductsByState(state: string): Promise<ProducsByStateDataType>;
 };
