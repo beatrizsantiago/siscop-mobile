58d08d0d00472df8b6352cbcaa21c8f4fbee6961
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2025-07-14 19:39:40 -0300

    ✨ feat: implement Notification entity and repository for paginated notifications

---
+++ domain/repositories/NotificationRepository.ts
@@ -0,0 +1,10 @@
+import Notification from '@/domain/entities/Notification';
+import { DocumentSnapshot } from 'firebase/firestore';
+
+export interface NotificationRepository {
+  getAllPaginated(lastDoc?: DocumentSnapshot): Promise<{
+    list: Notification[];
+    lastDoc?: DocumentSnapshot;
+    hasMore: boolean;
+  }>;
+};
